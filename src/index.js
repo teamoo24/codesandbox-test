dddda2111922de34ca7be07eb294516833f64cdf
Han Kyungsoo <dripcoke24@gmail.com>
2023-08-27 02:20:15 +0000

    分割代入配列の勉強

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
-const myProfile = {
-  name: "じゃけぇ",
-  age: 28,
-};
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
 
-console.log(message1);
+// console.log(message1);
 
-const { name, age } = myProfile;
+// const { name, age } = myProfile;
 
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
 
-console.log(message2);
+// console.log(message2);
+
+// const myProfile = ["じゃけぇ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
