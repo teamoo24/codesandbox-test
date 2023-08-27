c83c3e8fa61b1edf075417effa45c355a51ed913
Han Kyungsoo <dripcoke24@gmail.com>
2023-08-27 02:16:26 +0000

    分割代入練習

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 //テンプレート文字列を用いた方法
-const name = "じゃけぇ";
-const age = 28;
+// const name = "じゃけぇ";
+// const age = 28;
 
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile = {
+  name: "じゃけぇ",
+  age: 28,
+};
+
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+
+console.log(message1);
+
+const { name, age } = myProfile;
+
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+
 console.log(message2);
